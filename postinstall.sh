b5a5bdd2209bf7e46301b5e651f74a26da01cc9b
hypedad <48036096+hypedad@users.noreply.github.com>
2020-09-04 13:10:51 -0700

    Update postinstall.sh
    Revoked KDE repo and added a few other utils.
    
    Patched HiFi

--- postinstall.sh
+++ postinstall.sh
@@ -3,11 +3,15 @@
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y exfat-utils fuse-exfat
 sudo dnf copr enable dawid/better_fonts
-sudo dnf copr enable zawertun/kde
+dnf copr enable gloriouseggroll/kernel 
 sudo dnf update --refresh -y
-sudo dnf install -y fontconfig-enhanced-defaults fontconfig-font-replacements libva libva-utils ffmpeg intel-media-driver alsa-tools patch git
+sudo dnf distro-sync
+sudo dnf install -y fontconfig-enhanced-defaults fontconfig-font-replacements ffmpeg alsa-tools patch git wget htop gnome-tweaks evolution xrdp openssh
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/materia-kde/master/install.sh | sh
 wget -qO- https://git.io/papirus-folders-install | sh
+systemctl enable xrdp sshd
+cp HiFi.conf.dist /usr/share/alsa/ucm2/sof-hda-dsp/HiFi.conf.dist
+patch < /usr/share/alsa/ucm2/sof-hda-dsp/HiFi.conf.dist
 
 
